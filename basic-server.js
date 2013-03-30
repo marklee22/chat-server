7f2ab4737fbf0ec03ba38625d75a6f26b46b3638
Hack Reactor Students <accounts@hackreactor.com>
2013-03-30 16:44:37 -0700

    Implemented handleRequest function to properly set statusCodes

--- basic-server.js
+++ basic-server.js
@@ -1,5 +1,6 @@
 /* Import node's http module: */
 var http = require("http");
+var handler = require('./request-handler');
 
 /* This is the callback function that will be called each time a
  * client (i.e.. a web browser) makes a request to our server. */
@@ -20,19 +21,11 @@ var requestListener = function (request, response) {
 
   headers['Content-Type'] = "text/plain";
 
-  /* Response is an http.ServerRespone object containing methods for
-   * writing our response to the client. Documentation for both request
-   * and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html*/
-  response.writeHead(statusCode, headers);
-  /* .writeHead() tells our server what HTTP status code to send back
-   * to the client, and what headers to include on the response. */
-
   /* Make sure to always call response.end() - Node will not send
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+  response = handler.handleRequest(request, response, headers);
 };
 
 /* These headers will allow Cross-Origin Resource Sharing.
--- request-handler.js
+++ request-handler.js
@@ -4,6 +4,45 @@
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
 
-var handleRequest = function(request, response) {
 
+var handleRequest = function(request, response, headers) {
+  var statusCode, data;
+  if(require('url').parse(request.url).path === '/classes/room1') {
+    if(request.method === 'GET') statusCode = 200;
+    if(request.method === 'POST') statusCode = 302;
+  } else {
+    statusCode = 404;
+  }
+
+  if(require('url').parse(request.url).path === '/classes/messages') {
+    if(request.method === 'GET') {
+      statusCode = 200;
+      request.on('data', function(data) {
+        console.log(data);
+      });
+      // data = JSON.parse(request.form);
+    }
+    if(request.method === 'POST') {
+      statusCode = 302;
+      request.on('data', function(data) {
+        console.log(data);
+      });
+      data = '{}';
+    }
+  } else {
+    statusCode = 404;
+  }
+
+  /* Response is an http.ServerRespone object containing methods for
+   * writing our response to the client. Documentation for both request
+   * and response can be found at
+   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html*/
+  response.writeHead(statusCode, headers);
+  /* .writeHead() tells our server what HTTP status code to send back
+   * to the client, and what headers to include on the response. */
+
+  response.write(data);
+  response.end();
 };
+
+exports.handleRequest = handleRequest;
