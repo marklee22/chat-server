dd9803cfdfcd3f0ef1ae6e46817fe0d740ed0580
Hack Reactor Students <accounts@hackreactor.com>
2013-03-30 20:27:55 -0700

    Passing ServerSpec.js tests

--- request-handler.js
+++ request-handler.js
@@ -3,46 +3,38 @@
  * basic-server.js.  So you must figure out how to export the function
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
-
+var storage = require('./node_modules/node-persist/persist')
+storage.initSync();
+storage.setItem('length', 0);
 
 var handleRequest = function(request, response, headers) {
-  var statusCode, data;
-  if(require('url').parse(request.url).path === '/classes/room1') {
-    if(request.method === 'GET') statusCode = 200;
-    if(request.method === 'POST') statusCode = 302;
-  } else {
-    statusCode = 404;
-  }
+  var statusCode, retData;
 
-  if(require('url').parse(request.url).path === '/classes/messages') {
+  if(require('url').parse(request.url).path === '/classes/room1') {
     if(request.method === 'GET') {
       statusCode = 200;
-      request.on('data', function(data) {
-        console.log(data);
-      });
-      // data = JSON.parse(request.form);
+      // TODO: allow for more than one element for storage
+      retData = storage.getItem('length') === 0 ? '[]' : storage.getItem('data');
     }
     if(request.method === 'POST') {
       statusCode = 302;
-      request.on('data', function(data) {
-        console.log(data);
-      });
-      data = '{}';
+      retData = "\n";
+      storage.setItem('data', JSON.stringify([request._postData]));
+      storage.setItem('length', 1);
+    }
+  } else if(require('url').parse(request.url).path === '/classes/messages') {
+    if(request.method === 'GET') {
+      statusCode = 200;
+    }
+    if(request.method === 'POST') {
+      statusCode = 302;
     }
   } else {
     statusCode = 404;
   }
 
-  /* Response is an http.ServerRespone object containing methods for
-   * writing our response to the client. Documentation for both request
-   * and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html*/
   response.writeHead(statusCode, headers);
-  /* .writeHead() tells our server what HTTP status code to send back
-   * to the client, and what headers to include on the response. */
-
-  response.write(data);
-  response.end();
+  response.end(retData);
 };
 
 exports.handleRequest = handleRequest;
